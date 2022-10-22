41ae7807242a9d890b2b88145d05b67b6996f5ad
RealCryZed <bujhmgtnhjd@bk.ru>
2022-10-22 10:14:59 +0600

    Moved some methods from Main to DBService class

--- src/main/java/clangame/Main.java
+++ src/main/java/clangame/Main.java
@@ -1,11 +1,6 @@
 package clangame;
 
-import clangame.config.JdbcConnection;
 import clangame.service.*;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
@@ -14,68 +9,9 @@ import java.util.concurrent.Executors;
 public class Main {
 
     public static void main(String[] args) {
-        initializeTables();
+        DBService.initializeTables();
         simulateGoldAddition();
-        getDbInfo();
-    }
-
-    // Initialization of tables in PostgreSQL on external server
-    private static void initializeTables() {
-        Connection con = null;
-        Statement statement = null;
-        try {
-            con = JdbcConnection.getConnection();
-
-            String clansInitializationQuery =
-                    "drop table if exists clans;" +
-                    "create table clans (\n" +
-                    "    id int PRIMARY KEY,\n" +
-                    "    name VARCHAR(50) not null unique,\n" +
-                    "    gold int not null\n" +
-                    ");" +
-                    "insert into clans values(1, 'Clan1', 0);" +
-                    "insert into clans values(2, 'Clan2', 0);" +
-                    "insert into clans values(3, 'Clan3', 0);";
-
-            String goldFromTaskInitializationQuery =
-                    "drop table if exists gold_from_task;" +
-                    "create table gold_from_task (\n" +
-                    "    transaction_id int PRIMARY KEY generated always as identity,\n" +
-                    "    task_id int not null,\n" +
-                    "    clan_id int not null,\n" +
-                    "    initial_gold_value int not null,\n" +
-                    "    final_gold_value int not null,\n" +
-                    "    added_gold int not null);";
-
-            String goldDonationInitializationQuery =
-                    "drop table if exists gold_from_donation;" +
-                    "create table gold_from_donation (\n" +
-                    "    transaction_id int PRIMARY KEY generated always as identity,\n" +
-                    "    user_id int not null,\n" +
-                    "    clan_id int not null,\n" +
-                    "    initial_gold_value int not null,\n" +
-                    "    final_gold_value int not null,\n" +
-                    "    added_gold int not null);";
-
-            statement = con.createStatement();
-
-            statement.execute(clansInitializationQuery);
-            statement.execute(goldFromTaskInitializationQuery);
-            statement.execute(goldDonationInitializationQuery);
-
-            System.out.println("All tables initialized!\n");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (con != null && statement != null) {
-                    con.close();
-                    statement.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        DBService.getDbInfo();
     }
 
     // Simulation of adding gold from 2 different services at the same time
@@ -103,33 +39,6 @@ public class Main {
         } finally {
             depositExecService.shutdown();
             taskExecService.shutdown();
-        }
-    }
-
-    private static void getDbInfo() {
-        Scanner scanner = new Scanner(System.in);
-
-        while(true) {
-            System.out.println("Type 'tasks' to show all transactions made by completing tasks.");
-            System.out.println("Type 'deposits' to show all transactions made by donations.");
-            System.out.println("Type 'clans' to show all clans.");
-            System.out.println();
-
-            String userValue = scanner.nextLine();
-            switch (userValue.toLowerCase()) {
-                case "tasks":
-                    DBService.getAllTaskTransactions();
-                    break;
-                case "deposits":
-                    DBService.getAllDepositTransactions();
-                    break;
-                case "clans":
-                    DBService.getAllClans();
-                    break;
-                default:
-                    System.out.println("Try again!\n");
-                    break;
-            }
         }
     }
 }
--- src/main/java/clangame/service/DBService.java
+++ src/main/java/clangame/service/DBService.java
@@ -6,8 +6,95 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class DBService {
+
+    // Initialization of tables in PostgreSQL on external server
+    public static void initializeTables() {
+        Connection con = null;
+        Statement statement = null;
+        try {
+            con = JdbcConnection.getConnection();
+
+            String clansInitializationQuery =
+                    "drop table if exists clans;" +
+                            "create table clans (\n" +
+                            "    id int PRIMARY KEY,\n" +
+                            "    name VARCHAR(50) not null unique,\n" +
+                            "    gold int not null\n" +
+                            ");" +
+                            "insert into clans values(1, 'Clan1', 0);" +
+                            "insert into clans values(2, 'Clan2', 0);" +
+                            "insert into clans values(3, 'Clan3', 0);";
+
+            String goldFromTaskInitializationQuery =
+                    "drop table if exists gold_from_task;" +
+                            "create table gold_from_task (\n" +
+                            "    transaction_id int PRIMARY KEY generated always as identity,\n" +
+                            "    task_id int not null,\n" +
+                            "    clan_id int not null,\n" +
+                            "    initial_gold_value int not null,\n" +
+                            "    final_gold_value int not null,\n" +
+                            "    added_gold int not null);";
+
+            String goldDonationInitializationQuery =
+                    "drop table if exists gold_from_donation;" +
+                            "create table gold_from_donation (\n" +
+                            "    transaction_id int PRIMARY KEY generated always as identity,\n" +
+                            "    user_id int not null,\n" +
+                            "    clan_id int not null,\n" +
+                            "    initial_gold_value int not null,\n" +
+                            "    final_gold_value int not null,\n" +
+                            "    added_gold int not null);";
+
+            statement = con.createStatement();
+
+            statement.execute(clansInitializationQuery);
+            statement.execute(goldFromTaskInitializationQuery);
+            statement.execute(goldDonationInitializationQuery);
+
+            System.out.println("All tables initialized!\n");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (con != null && statement != null) {
+                    con.close();
+                    statement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void getDbInfo() {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Type 'tasks' to show all transactions made by completing tasks.");
+            System.out.println("Type 'deposits' to show all transactions made by donations.");
+            System.out.println("Type 'clans' to show all clans.");
+            System.out.println();
+
+            String userValue = scanner.nextLine();
+            switch (userValue.toLowerCase()) {
+                case "tasks":
+                    DBService.getAllTaskTransactions();
+                    break;
+                case "deposits":
+                    DBService.getAllDepositTransactions();
+                    break;
+                case "clans":
+                    DBService.getAllClans();
+                    break;
+                default:
+                    System.out.println("Try again!\n");
+                    break;
+            }
+        }
+    }
 
     public static void getAllClans() {
         Connection con = null;
